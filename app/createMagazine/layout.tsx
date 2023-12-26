2cc0d6a2fe60d37026ba72cf171f60f137018b4c
Suvodip Chakraborty <chakrabortysuvodip3@gmail.com>
2023-12-27 02:44:39 +0530

    Sample Verification test

--- app/createMagazine/layout.tsx
+++ app/createMagazine/layout.tsx
@@ -1,26 +1,26 @@
 import clsx from "clsx";
-import { Fira_Code as FontMono, Inter as FontSans } from "next/font/google"
+import { Fira_Code as FontMono, Inter as FontSans } from "next/font/google";
 
 export const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
-})
+});
 export default function AboutLayout({
-	children,
+  children,
 }: {
-	children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-	return (
-		<div className={clsx(
-					"min-h-screen bg-background font-sans antialiased",
-					fontSans.variable
-				)}>
-		<section className="py-8 md:py-10">
-			<div className="">
-				{children}
-			</div>
-		</section>
-		</div>
-
-	);
+  return (
+    <div
+      className={clsx(
+        "min-h-screen bg-background font-sans antialiased",
+        fontSans.variable
+      )}
+    >
+      <section className="py-8 md:py-10">
+        <div className="">{children}</div>
+      </section>
+    </div>
+    //sample commit
+  );
 }
